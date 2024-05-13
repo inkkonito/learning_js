94c31074688cb36c267e4f768b617c80a50c0446
inkkonito <pro.julienp@gmail.com>
2024-05-13 18:42:46 +0400

    init base devs

---
+++ base/script.js
@@ -0,0 +1,15 @@
+var a = 1;
+var b = "hello reunion";
+var c;
+var d = [];
+var e = ["Un", "Deux", "Trois"];
+var f = { "cle": "valeur" };
+var g = true;
+
+console.log("1 = " + typeof a);
+console.log("Chaine \"hello reunion\" = " + typeof b);
+console.log("Variable seulement déclarée = " + typeof c);
+console.log("Tableau vide = " + typeof d);
+console.log("Tableau à trois entrées = " + typeof e);
+console.log("Objet JSON = " + typeof f);
+console.log("Booléen à valeur TRUE = " + typeof g);
