0583969d5c0e0107d7b59d791f37e8995564a8ef
inkkonito <pro.julienp@gmail.com>
2024-05-25 12:58:56 +0200

    arrays functions

--- from_book/part7.js
+++ from_book/part7.js
@@ -71,6 +71,9 @@ clients = [
 ];
 
 console.table(clients);
+// get valeur from object using doted notation
+
+console.log("L'âge du premier client est " + clients[0].age);
 
 // methodes on arrays
 
@@ -90,7 +93,6 @@ console.log(
   })
 );
 
-
 if (
   tableau3.every(function (val) {
     return val.charAt(0) === "J";
@@ -103,48 +105,62 @@ if (
 
 // every 2nd exercice
 
-var blues = ["gris Bleu", "Bleu clair", "Bleu très clair"]
-var blues2 = ["gris bleu", "Bleu clair", "Bleu très clair"]
+var blues = ["gris Bleu", "Bleu clair", "Bleu très clair"];
+var blues2 = ["gris bleu", "Bleu clair", "Bleu très clair"];
 var regex = /bleu/i;
 
-console.log(blues.every(function(e) {
-     return (e.indexOf("Bleu") >= 0)
-})) // all items contain "Blue
+console.log(
+  blues.every(function (e) {
+    return e.indexOf("Bleu") >= 0;
+  })
+); // all items contain "Blue
 
-console.log(blues2.every(function(e) {
+console.log(
+  blues2.every(function (e) {
     return regex.test(e);
-})); // all items match the regex "blue" with i option
+  })
+); // all items match the regex "blue" with i option
 
 // filter
 
 var pairs = [0, 2, 4, 5, 8, 10];
 
-console.log(pairs.filter(function(val) {
-    return (val%2==0)
-}));
+console.log(
+  pairs.filter(function (val) {
+    return val % 2 == 0;
+  })
+);
 
 var shortNames = ["Max", "Jean", "Paul", "Julien", "Jerome"];
 console.log(shortNames[0].length); // Max length is 3
 
-console.log(shortNames.filter(function(val) {
-    return val.length<=4 // return only names Max, Jean, Paul because length is <= 4
-}))
+console.log(
+  shortNames.filter(function (val) {
+    return val.length <= 4; // return only names Max, Jean, Paul because length is <= 4
+  })
+);
 
 // find
 
-console.log(shortNames.find(function(val) {
-    return val.charAt(0) === "J" // return Jean (first J)
-}))
+console.log(
+  shortNames.find(function (val) {
+    return val.charAt(0) === "P"; // return Jean (first J)
+  })
+);
 
-console.log(shortNames.find(function(val) {
+console.log(
+  shortNames.find(function (val) {
     return val.length > 4; // return only Julien (first length 4)
-}))
+  })
+);
 
 // findIndex
 
-console.log(shortNames.findIndex(function(val) {
+console.log(
+  shortNames.findIndex(function (val) {
     return val === "Julien"; // return 3 as Julin is in 3rd position
-}))
+  })
+);
 
 // flat
 
@@ -152,3 +168,31 @@ var codes = [1, 2, [3, 4], 5, [6, 7, 8]];
 console.table(codes);
 
 console.log(codes.flat());
+
+// for each
+
+var days = ["lun", "mar", "mer", "jeu", "ven", "sam", "dim"];
+
+days.forEach(function (day, index, table) {
+  table[index] = day.toUpperCase();
+});
+
+console.log(days.join(", "));
+
+var numbers = [0, 2, 4, 6, 8, 8];
+
+numbers.forEach(function (number, index, table) {
+  table[index] = number + 1; // add one to each elem in numbers array
+});
+
+console.log(numbers.toString()); // return 1, 3, 5, 7, 9
+
+console.log(numbers.includes(3)); // return 3 because exist
+
+// indexof 
+
+console.log(numbers.indexOf(3)); // return 1 as 3 is in second position
+
+// lastIndexOf
+
+console.log(numbers.lastIndexOf(9)); // return 5 as there are two 9 (1, 3, 5, 7, 9, 9) and the last one is in 5th position
