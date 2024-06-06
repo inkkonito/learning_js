0e86cb8266abf9deb3df54d367b1e753fc4c2e08
inkkonito <pro.julienp@gmail.com>
2024-06-06 22:03:34 +0200

    playing with html and css JS

--- from_book/part12.js
+++ from_book/part12.js
@@ -123,3 +123,110 @@ return longestWord;
 
 console.log(find_longest_word("Justine aime beaucoup Julien"));
 
+// redo 
+
+function inverse_string(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(inverse_string("coucou non"));
+
+
+// research element in nodes 
+
+console.log(document.getElementsByTagName("p"));
+console.log(document.getElementsByClassName("para"));
+console.log(document.getElementsByName("img"));
+console.log(document.getElementById("bloc"));
+console.log(document.getElementById("myId"));
+
+// access with CSS selectors
+
+console.log(document.querySelectorAll("p"));
+console.log(document.querySelectorAll("span.span"));
+console.log(document.querySelectorAll("div#bloc"));
+
+// direct access
+
+console.log(document.body);
+console.log(document.getElementsByTagName("body")); // equals
+
+// access child nodes - firstChild lastchild and siblings
+
+console.log(document.querySelector("div#bloc").childNodes);
+console.log(document.querySelector("div#bloc").firstChild);
+console.log(document.querySelector("div#bloc").lastChild);
+console.log(document.querySelector("div span").nextElementSibling);
+console.log(document.querySelector("span#spaninbloc").parentElement);
+
+// modifying the content
+
+console.log(document.querySelectorAll("div#bloc > p")[0].innerHTML); // get Para A in bloc html
+console.log(document.querySelectorAll("div#bloc > p")[0].outerHTML); // including tag
+console.log(document.querySelectorAll("div#bloc > p")[0].innerText); // without HTML
+
+document.querySelectorAll("div#bloc > p")[0].innerHTML = "this is a modification";
+console.log(document.querySelectorAll("div#bloc > p")[0].innerHTML); // shows "this is a modification"
+
+// test ajouter des évènements
+
+var paras = document.querySelectorAll("p");
+console.log(paras);
+for(var i=0; i<paras.length ; i++) {
+    paras[i].addEventListener("click", function(evt) {
+        console.log("Clic détecté sur " + evt.target);
+    })
+}
+
+paras = Array.prototype.slice.call(paras); // use 
+console.log(paras); // array
+console.log(Array.isArray(paras)); // true
+paras.forEach(function(p) {
+    p.addEventListener("click", function(evt)
+{    console.log("Click détecté sur" + evt.target+ "(déclaration via ForEach");
+})
+});
+
+// accéder aux attributs
+
+getImg = document.querySelectorAll("img")[0];
+console.log(getImg);
+
+console.log(getImg.hasAttribute("alt")); // true
+console.log(getImg.getAttribute("alt")); // return "alternative text"
+console.log(getImg.hasAttribute("size")); // false
+getImg.setAttribute("alt", "nouveau contenu"); // change alt content
+console.log(getImg.getAttribute("alt")); // return "nouveau contenu"
+
+// CSS manipulation - get style
+
+console.log(window.getComputedStyle(getImg));
+console.log(window.getComputedStyle(document.getElementById("monH1")));
+
+// ajouter des class 
+
+var ps = document.getElementById("monH1");
+console.log(getComputedStyle(ps));
+
+console.log(document.querySelector("p.para.paraBold").classList);
+console.log(document.querySelector("p.para.paraBold").classList.add("test")); // added class test
+console.log(document.querySelector("p.para.paraBold").classList);
+
+// la position
+
+console.log(ps.getBoundingClientRect());
+
+// create element
+
+var newDiv = document.createElement("div");
+newDiv.setAttribute("id", "notification");
+newDiv.innerHTML = "notre bloc de text";
+newDiv.className = "newClass";
+console.log(newDiv);
+
+// clone element
+
+var newDiv2 = newDiv.cloneNode(true);
+newDiv2.innerHTML = "text du div 2";
+console.log(newDiv2);
+body.appendChilde(newDiv2);
