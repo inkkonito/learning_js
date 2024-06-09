7f74a66d98cf14c2a6bef0f30dc901585cb0c022
inkkonito <pro.julienp@gmail.com>
2024-06-09 17:41:36 +0200

    playing with dom elements

--- from_book/part13.js
+++ from_book/part13.js
@@ -37,3 +37,58 @@ if (document.monForm.saisie.value.length>3) {
     console.log("la valeur est plus longue que 6");
 }
 console.log(document.monForm.saisie.value.length>5); // true
+
+// hidden fields
+
+var reso = document.getElementById("resolution");
+var screenWidth = window.screen.width;
+var screenHeight = window.screen.height;
+console.log(screenWidth);
+console.log(screenHeight);
+reso.value = `${screenHeight} + ${screenWidth}`;
+console.log(reso); // show value
+
+// password
+var inputPass = document.getElementById("pass");
+console.log(typeof pass.type);
+
+document.getElementById("pass").addEventListener("click", function (evt) {
+  if(pass.type === "password")
+{    evt.target.setAttribute("type", "text");
+} else {
+    evt.target.setAttribute("type", "password")
+}
+})
+
+inputCheck = document.getElementById("regles");
+console.log(inputCheck.value);
+
+if (!inputCheck.checked) {
+    alert("Vous devez valider la checkbox");
+}
+
+var animal = "";
+console.log(document.monForm.animal); // radio node list
+console.log(document.monForm.animal.length); // radio node length
+
+console.log(typeof animal);
+
+
+for (var i = 0; i < document.monForm.animal.length; i++) {
+    if (document.monForm.animal[i].checked) {
+        animal = document.monForm.animal[i].value;
+        console.log(animal);
+        break;
+    }
+}
+
+if (animal === "") { 
+    console.log("Veuillez saisir votre animal préféré");
+} else {
+    console.log(animal);
+}
+
+var listFilm = document.monForm.film;
+console.log(listFilm);
+console.log(listFilm.options); // list options
+console.log(listFilm.options[1].value); // show option value using index
